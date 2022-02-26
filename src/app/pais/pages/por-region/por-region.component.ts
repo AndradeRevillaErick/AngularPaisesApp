ae3bf0eb74335c4f8ab8ec98f6f19edc2b1f3fc0
Erick Andrade <erick_andradhe@hotmail.com>
2022-02-25 21:28:10 -0600

    mejoras app paises, busqueda por region

--- src/app/pais/pages/por-region/por-region.component.ts
+++ src/app/pais/pages/por-region/por-region.component.ts
@@ -1,16 +1,57 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Country } from '../../interfaces/pais.interface';
+import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-por-region',
   templateUrl: './por-region.component.html',
-  styles: [
-  ]
+  styles: [`
+    button {
+      margin-right: 5px;
+    }
+  `]
 })
-export class PorRegionComponent implements OnInit {
+export class PorRegionComponent {
 
-  constructor() { }
+  regiones: string[] = ['africa', 'americas', 'asia', 'europe', 'oceania'];
+  regionActiva: string = '';
 
-  ngOnInit(): void {
+  termino: string = '';
+  hayError: boolean = false;
+  paises: Country[] = [];
+
+  constructor(private paisService: PaisService) { }
+
+  getClaseCSS(region: string): string{
+    return (region === this.regionActiva) ? 'btn btn-primary' : 'btn btn-outline-primary';
   }
 
+  activaRegion(region: string){
+
+    if(region === this.regionActiva){return;}
+
+    this.regionActiva = region;
+    this.paises = [];
+
+    this.hayError = false;
+    this.termino = region;
+
+    this.paisService.getPaisPorRegion(this.termino)
+      .subscribe({
+        next: (paises) => { 
+          console.log(paises);
+          this.paises = paises;
+          console.log(this.paises);
+        },
+        error: (err) => {
+          this.hayError = true;
+          this.paises = [];
+          console.info(err);
+        } 
+      });
+      
+    console.log(this.termino);
+
+  }
+  
 }
--- src/app/pais/services/pais.service.ts
+++ src/app/pais/services/pais.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Country } from '../interfaces/pais.interface';
 //import { catchError, Observable,of } from 'rxjs'; solo p[ara el ejemplo de pipse rxjs
@@ -11,24 +11,38 @@ export class PaisService {
 
   private apiUrl: string = "https://restcountries.com/v3.1";
 
+  get httpParams(){
+    return new HttpParams().set( 'fields', 'name,capital,population,flags,cca2' );
+  }
+
   constructor(private http: HttpClient) { }
 
   buscarPais( termino: string): Observable<Country[]>{
     
     const url = `${ this.apiUrl }/name/${ termino }`;
 
-    return this.http.get<Country[]>(url);
+    return this.http.get<Country[]>(url, {params: this.httpParams});
     //Ejemplo para probar el uso de pipes rxjs para regresar un arreglo con un mensaje
     //.pipe(catchError((err) => of(['Hola hay un error bro'])));
   }
 
   buscarCapital( termino: string): Observable<Country[]>{
     const url = `${ this.apiUrl }/capital/${ termino }`;
-    return this.http.get<Country[]>(url);
+    return this.http.get<Country[]>(url, {params: this.httpParams});
   }
 
   getPaisPorAlpha( termino: string): Observable<Country>{
     const url = `${ this.apiUrl }/alpha/${ termino }`;
     return this.http.get<Country>(url);
+  }
+
+  getPaisPorRegion( termino: string): Observable<Country[]>{
+
+    const url = `${ this.apiUrl }/region/${ termino }`;
+
+    //en emsc6 si se usa una variable que se llama igual al tipo de dato
+    //se puede obviar y en lugar de escribirse asi params: params seria asi params 
+    // por ejemplo los HttpParams creados son de tipo params
+    return this.http.get<Country[]>( url, {params: this.httpParams});
   }
 }
