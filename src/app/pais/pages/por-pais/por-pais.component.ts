1f7bf1ad3dfb8d2ad291f51c0025d59664182206
Erick Andrade <erick_andradhe@hotmail.com>
2022-02-28 22:23:14 -0600

    add animaciones y sugerencias de busqueda

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -6,6 +6,11 @@ import { Country, Currencies } from '../../interfaces/pais.interface';
   selector: 'app-por-pais',
   templateUrl: './por-pais.component.html',
   styles: [
+    `
+    li{
+      cursor: pointer;
+    }
+    `
   ]
 })
 export class PorPaisComponent {
@@ -13,6 +18,9 @@ export class PorPaisComponent {
   termino: string = '';
   hayError: boolean = false;
   paises: Country[] = [];
+
+  paisesSugeridos: Country[] = [];
+  mostrarSugerencias: boolean = false;
 
   constructor(private paisService: PaisService) { }
 
@@ -40,6 +48,18 @@ export class PorPaisComponent {
 
   sugerencias(termino: string){
     this.hayError = false;
+    this.termino = termino;
+    this.mostrarSugerencias = true;
 
+    this.paisService.buscarPais( termino )
+      .subscribe( 
+        paises => this.paisesSugeridos = paises.splice(0,5),
+        (err) => this.paisesSugeridos = []
+      );
+
+  }
+
+  buscarSugerido(termino: string){
+    this.buscar(termino);
   }
 }
